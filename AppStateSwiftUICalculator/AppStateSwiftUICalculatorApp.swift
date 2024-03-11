74847953062f4320e5978bc7f6360a82e1a05b1e
Roy <eduardo_rodrigues@epam.com>
2024-03-11 16:23:59 -0300

    Initial Commit

---
+++ AppStateSwiftUICalculator/AppStateSwiftUICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStateSwiftUICalculatorApp.swift
+//  AppStateSwiftUICalculator
+//
+//  Created by Eduardo Rodrigues on 11/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AppStateSwiftUICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
